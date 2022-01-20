0dc6c13c75564ed2e8c3e90f709f5e661aab1d6d
Anna Rotkiel-Kozik <anna.rotkiel@wp.pl>
2022-01-20 22:07:53 +0100

    Add remove in initActions

--- src/js/script.js
+++ src/js/script.js
@@ -30,25 +30,34 @@
       bookListContainer.appendChild(element);
     }
   }
- 
+
   function initActions() {
-    const thisBook = this;
     const favoriteBooks = [];
     const bookImages = document.querySelectorAll(select.containerOf.bookImage);
 
     for(let image of bookImages) {
-        image.addEventListener('dblclick', function (event){
-            event.preventDefault();
-            image.classList.add('favorite');
-            const bookId = thisBook.bookList.getAttribute('data-id');
-            favoriteBooks.push(bookId);
-          });
+      image.addEventListener('dblclick', function (event){
+        event.preventDefault();
+        const bookId = image.getAttribute('data-id'); 
+
+        if(favoriteBooks.includes(bookId)){
+          image.classList.remove('favorite');
+          const indexOfImage = favoriteBooks.indexOf(bookId);
+          favoriteBooks.splice(indexOfImage, 1);
+        } else {
+          image.classList.add('favorite');
+          favoriteBooks.push(bookId);
+        }
+
+      });
     }
   }
-
+ 
+  
 
   render ();
   initActions();
 
+  
 
 }
